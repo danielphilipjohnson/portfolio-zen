3aa4e774726d4ef8383f9d1031e7ab28408edfa6
Daniel Philip Johnson <daniel-philip-johnson@outlook.com>
2025-05-02 22:21:33 +0100

    feat: add ScrollToTop component to reset scroll position on route change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ZenNavbar from "@/components/layouts/Navbar";
 import ZenFooter from "@/components/layouts/Footer";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ZenNavbar />
+        <ScrollToTop />
         {children}
         <ZenFooter />
       </body>
