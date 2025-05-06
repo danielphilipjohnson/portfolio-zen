92432c74cdd223f170de0b0d47c18040d9e566e5
Daniel Philip Johnson <daniel-philip-johnson@outlook.com>
2025-05-06 22:28:31 +0100

    feat: refactor BlogPage component to enhance SEO and structured data integration by moving logic to a dedicated BlogPage file

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,6 @@
 import { getPageSEO } from "@/utils/seo";
 import { Metadata } from "next";
-import { getAllBlogs } from '@/lib/content/getAllBlogs';
-import BlogPageClient from '@/components/blog/BlogPageClient';
-import { format, isValid, parse } from 'date-fns';
-import { getBlogListingPageJsonLd } from '@/utils/jsonLd';
-import StructuredData from "@/components/StructuredData";
+
 
 export async function generateMetadata(): Promise<Metadata> {
 	const seoData = getPageSEO('/blog');
@@ -31,27 +27,4 @@ export async function generateMetadata(): Promise<Metadata> {
 	};
 }
 
-const BlogPage = async () => {
-	const blogPosts = await getAllBlogs();
-
-	const postSummaries = blogPosts.map(post => {
-		const date = parse(post.publishDate, 'dd/MM/yyyy', new Date());
-		return {
-			slug: post.slug,
-			title: post.title,
-			publishDate: isValid(date) ? format(date, 'yyyy-MM-dd') : 'Invalid date',
-			imageUrl: post.imageUrl,
-			excerpt: post.excerpt,
-		};
-	});
-	const blogListingJsonLdData = getBlogListingPageJsonLd(postSummaries);
-
-	return (
-		<>
-			<StructuredData data={blogListingJsonLdData} id="blog-listing-jsonld" />
-			<BlogPageClient initialBlogPosts={blogPosts} />
-		</>
-	);
-};
-
-export default BlogPage;
+export { default } from "@/components/pages/BlogPage";
--- src/components/pages/BlogPage.tsx
+++ src/components/pages/BlogPage.tsx
@@ -1,9 +1,29 @@
 import { getAllBlogs } from '@/lib/content/getAllBlogs';
 import BlogPageClient from '@/components/blog/BlogPageClient';
+import { format, isValid, parse } from 'date-fns';
+import { getBlogListingPageJsonLd } from '@/utils/jsonLd';
+import StructuredData from '../StructuredData';
 
 const BlogPage = async () => {
 	const blogPosts = await getAllBlogs();
-	return <BlogPageClient initialBlogPosts={blogPosts} />;
-};
+
+	const postSummaries = blogPosts.map(post => {
+		const date = parse(post.publishDate, 'dd/MM/yyyy', new Date());
+		return {
+			slug: post.slug,
+			title: post.title,
+			publishDate: isValid(date) ? format(date, 'yyyy-MM-dd') : 'Invalid date',
+			imageUrl: post.imageUrl,
+			excerpt: post.excerpt,
+		};
+	});
+	const blogListingJsonLdData = getBlogListingPageJsonLd(postSummaries);
+
+	return (
+		<>
+			<StructuredData data={blogListingJsonLdData} id="blog-listing-jsonld" />
+			<BlogPageClient initialBlogPosts={blogPosts} />
+		</>
+	); };
 
 export default BlogPage;
